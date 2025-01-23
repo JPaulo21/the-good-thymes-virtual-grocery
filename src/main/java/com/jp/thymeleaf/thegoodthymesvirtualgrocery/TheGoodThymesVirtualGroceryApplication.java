cb6da58e7dd42ea2a86f9f67d86cc3829a236b61
João Paulo <jp.sport17@gmail.com>
2025-01-23 12:01:53 -0300

    feat: Log para mostrar endereço da aplicação

--- src/main/java/com/jp/thymeleaf/thegoodthymesvirtualgrocery/TheGoodThymesVirtualGroceryApplication.java
+++ src/main/java/com/jp/thymeleaf/thegoodthymesvirtualgrocery/TheGoodThymesVirtualGroceryApplication.java
@@ -1,15 +1,38 @@
 package com.jp.thymeleaf.thegoodthymesvirtualgrocery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @EnableFeignClients
 @SpringBootApplication
 public class TheGoodThymesVirtualGroceryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TheGoodThymesVirtualGroceryApplication.class, args);
+	private static final Logger log = LoggerFactory.getLogger(TheGoodThymesVirtualGroceryApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(TheGoodThymesVirtualGroceryApplication.class);
+		Environment env = app.run(args).getEnvironment();
+
+		String info = """
+				\n-----------------------------------------------------------------------------------------------------------------------------------------------
+				Application {} is running!
+				-----------------------------------------------------------------------------------------------------------------------------------------------
+					Access URLs:
+						Local: \t\thttp://localhost:{}
+						External: \thttp://{}:{}
+				-----------------------------------------------------------------------------------------------------------------------------------------------
+				""";
+		log.info(info, env.getProperty("spring.application.name"),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
 
 }
